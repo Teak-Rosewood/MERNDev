cb29c26de1b240d7be00ff5da8ca5854aa15df91
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2023-12-03 20:10:49 +0530

    week 1.1

--- week_1/1_2.js
+++ week_1/1_2.js
@@ -1,3 +1,5 @@
+// Question Set 1 
+
 const prompt = require("prompt-sync")();
 let f_name = prompt("Enter you first name: ");
 let l_name = prompt("Enter you first name: ");
@@ -8,8 +10,32 @@ if(gender == 1){
 else{
     console.log("Your name is", f_name, l_name, "and you are a Female");
 }
-count = 0;
-for (let i = 0; i < 100; i++){
-    count = count + i;
+let count = 0;
+for (let i = 0; i <= 100; i++){
+    count += i;
 }
 console.log("Sum of 100 numbers: ", count);
+
+
+// Question Set 2
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+biggest_num = nums[0];
+for (i in nums){
+    if(nums[i]%2 == 0)
+    {
+        console.log(nums[i]);
+    }
+    if(nums[i] > biggest_num){
+        biggest_num = nums[i];
+    }
+}
+
+console.log("Biggest number:", biggest_num)
+
+const person = {
+    f_name: "John",
+    l_name: "Doe", 
+    gender: 1
+};
+console.log(person.f_name)
