c0063feebf1a85df73b567b648ca90706ffcfe55
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2024-01-03 20:38:51 +0530

    update

---
+++ tailwind/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-vuejs border-4 border-green-800">Hello this is the first text</h1>
+            <h1 className="text-yellow-800">Hello this is the second text</h1>
+        </>
+    );
+}
